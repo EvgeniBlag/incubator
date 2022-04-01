3a76a3998b5213996484e69a70952bde801a8eea
EvgeniBlag <blagodarni@gmail.com>
2022-04-01 14:30:09 +0200

    probe

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,42 +1,42 @@
 import React from "react";
 
 
-type TodoListPropsType={ 
+type TodoListPropsType = {
 
-   topic1?:string
-   arr:Array<inArrayPropsType>
+    topic1?: string
+    arr: Array<inArrayPropsType>
 }
 
-type inArrayPropsType={
-    id:number
-    title:string
-    isDone:boolean
+type inArrayPropsType = {
+    id: number
+    title: string
+    isDone: boolean
 }
 
- export const TodoList =(props:TodoListPropsType)=> {
+export const TodoList = (props: TodoListPropsType) => {
     return (
-            <div className='TodoList'>
+        <div className='TodoList'>
             <h3>{props.topic1}</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                 {props.arr.map((el:inArrayPropsType)=>{
-                     return(
-                       <li> <input type="checkbox" checked={el.isDone}/> <span>{el.title}</span></li>
-                     )
-                 })}
-
-                 
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
+            <div>
+                <input />
+               
             </div>
-       
+            <ul>
+                {props.arr.map((el: inArrayPropsType) => {
+                    return (
+                        <li> <input type="checkbox" checked={el.isDone} /> <span>{el.title}</span></li>
+                    )
+                })}
+
+
+            </ul>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
+        </div>
+
     );
 }
 
